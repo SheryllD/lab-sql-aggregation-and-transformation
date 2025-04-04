24ea52eb4b6dc86f215a7c74f694487a1bbe90f4
Sheryll Dumapal <sheryllhhs@outlook.com>
2025-04-04 04:11:35 +0200

    completed lab

--- solved_lab-sql-aggregation-and-transformation.sql
+++ solved_lab-sql-aggregation-and-transformation.sql
@@ -55,11 +55,27 @@ LIMIT 20;
 -- If any rental duration value is NULL, replace it with the string 'Not Available'. Sort the results of the film title in ascending order.
 -- Please note that even if there are currently no null values in the rental duration column, the query should still be written to handle such cases in the future.
 -- Hint: Look for the IFNULL() function.
+SELECT 
+    title, 
+    IFNULL(CONVERT(rental_duration, CHAR), 'Not Available') AS rental_duration
+FROM 
+    film
+ORDER BY 
+    title ASC;
+
 
 -- 4. Bonus: The marketing team for the movie rental company now needs to create a personalized email campaign for customers. 
 -- To achieve this, you need to retrieve the concatenated first and last names of customers, along with the first 3 characters of their email address, 
 -- so that you can address them by their first name and use their email address to send personalized recommendations. 
 -- The results should be ordered by last name in ascending order to make it easier to use the data.
+
+SELECT
+	CONCAT(first_name, ' ', last_name) AS full_name,
+	LEFT(email, 3) AS email_prefix
+FROM 
+	customer 
+ORDER BY
+	last_name ASC;
 
 -- Challenge 2
 
@@ -69,6 +85,69 @@ LIMIT 20;
 -- 1.3 The number of films for each rating, sorting the results in descending order of the number of films. This will help you to better understand the popularity of different film ratings and adjust purchasing decisions accordingly.
 -- Using the film table, determine:
 
+-- 1.1 
+SELECT 
+	COUNT(*) AS total_films 
+FROM 
+	film;
+ 
+-- 1.2 
+
+SELECT 
+	rating, 
+    COUNT(*) AS number_of_films
+FROM 	
+	film
+GROUP BY 
+	rating; 
+
+-- 1.3 
+
+SELECT 
+	rating, 
+    COUNT(*) AS number_of_films
+FROM 
+	film 
+GROUP BY 
+	rating 
+ORDER BY 
+	number_of_films DESC;
+	
+
 -- 2.1 The mean film duration for each rating, and sort the results in descending order of the mean duration. Round off the average lengths to two decimal places. This will help identify popular movie lengths for each category.
 -- 2.2 Identify which ratings have a mean duration of over two hours in order to help select films for customers who prefer longer movies.
 -- 3. Bonus: determine which last names are not repeated in the table actor.
+
+-- 2.1 
+SELECT
+	rating, 
+    ROUND(AVG(length), 2) AS mean_duration 
+FROM film 
+GROUP by 
+	rating 
+ORDER BY 
+	mean_duration DESC;
+
+-- 2.2 
+
+SELECT 
+	rating, 
+    ROUND(AVG(length), 2) AS mean_duration 
+FROM 
+	film 
+GROUP BY 
+	rating
+HAVING 
+	AVG(length) >120
+ORDER BY
+	mean_duration DESC;
+    
+-- 3 Bonus 
+SELECT 
+	last_name 
+FROM 
+	actor 
+GROUP BY
+	last_name 
+HAVING 
+	COUNT(*) = 1
